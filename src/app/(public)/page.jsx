813c1225de1cda7731cab8ac92822902a6fae80b
Raza Fazal <razafazal19@gmail.com>
2025-04-17 23:31:55 +0500

    cvbn

--- src/app/(public)/page.jsx
+++ src/app/(public)/page.jsx
@@ -1,6 +1,11 @@
 import LoginPage from "./login/page";
 
 export default function Home() {
+
+
+
+
+
   return (
     <div className="flex flex-col items-center justify-center w-full px-4 py-8  ">
       <LoginPage />
